d5bfcaf3f1a54bdfac3fd0ea375e7c42a63e51d3
Nguyen Huy Dung <234769026+nguyenhdung2006@users.noreply.github.com>
2026-04-05 22:45:57 +0700

    lt6

---
+++ lt_6.sql
@@ -0,0 +1,24 @@
+CREATE OR REPLACE PROCEDURE update_product_price(
+    p_category_id INT,
+    p_increase_percent NUMERIC
+)
+LANGUAGE plpgsql
+AS $$
+DECLARE
+    r_product RECORD;
+    v_new_price NUMERIC;
+BEGIN
+    FOR r_product IN
+        SELECT product_id, price FROM Products WHERE category_id = p_category_id
+        LOOP
+            v_new_price := r_product.price * (1 + p_increase_percent / 100);
+
+            UPDATE Products
+            SET price = v_new_price
+            WHERE product_id = r_product.product_id;
+
+        END LOOP;
+
+    RAISE NOTICE 'Đã cập nhật xong: %', p_category_id;
+END;
+$$;
