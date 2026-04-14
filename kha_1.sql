e439576c351b5e708d86c265091a74c80ecc5c9e
Nguyen Huy Dung <234769026+nguyenhdung2006@users.noreply.github.com>
2026-04-14 12:21:26 +0700

    change content

--- kha_1.sql
+++ kha_1.sql
@@ -1,26 +1,44 @@
+CREATE TABLE flights (
+    flight_id SERIAL PRIMARY KEY ,
+    flight_name varchar(100) ,
+    avaiable_seats INT
+);
+
+CREATE TABLE bookings (
+    booking_id SERIAL primary key ,
+    flight_id INT REFERENCES flights(flight_id),
+    customer_name varchar(100)
+);
+
+-- 1.
+INSERT INTO flights(flight_name, avaiable_seats)
+VALUES ('VN123', 3) , ('VN456', 2);
+
 BEGIN;
+    -- xóa, giảm số ghế chuyến bay
+    UPDATE flights
+    SET avaiable_seats = avaiable_seats - 1
+    WHERE flight_name = 'VN123';
 
-UPDATE flights 
-SET available_seats = available_seats - 1 
-WHERE flight_name = 'VN123';
-
-INSERT INTO bookings (flight_id, customer_name) 
-VALUES ((SELECT flight_id FROM flights WHERE flight_name = 'VN123'), 'Nguyen Van A');
-
+    -- thêm bản ghi đặt vé của khách hàng 'Nguyen Van A
+    INSERT INTO bookings(booking_id, flight_id, customer_name)
+    VALUES (1, 1, 'Nguyen Van A');
 COMMIT;
 
-SELECT * FROM flights;
-SELECT * FROM bookings;
-
+-- 2.
 BEGIN;
 
-UPDATE flights 
-SET available_seats = available_seats - 1 
+-- giảm ghế (tạm thời)
+UPDATE flights
+SET avaiable_seats = avaiable_seats - 1
 WHERE flight_name = 'VN123';
 
-INSERT INTO bookings (flight_id, customer_name) 
-VALUES (346, 'Khach Hang Loi'); 
+-- cố tình sai flight_id (3 không tồn tại)
+INSERT INTO bookings(flight_id, customer_name)
+VALUES (3, 'Nguyen Van A');
 
+-- rollback toàn bộ
 ROLLBACK;
 
-SELECT * FROM flights WHERE flight_name = 'VN123';
+SELECT * FROM flights;
+
