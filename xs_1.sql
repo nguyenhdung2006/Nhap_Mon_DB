d215435289bac6f4b4ea4dfdf1c7ea830d295ed8
Nguyen Huy Dung <234769026+nguyenhdung2006@users.noreply.github.com>
2026-04-15 16:49:01 +0700

    c

--- xs_1.sql
+++ xs_1.sql
@@ -1,51 +1,101 @@
-BEGIN;
+CREATE TABLE customers (
+                           customer_id SERIAL PRIMARY KEY,
+                           name VARCHAR(100),
+                           balance NUMERIC(12,2)
+);
+
+CREATE TABLE products (
+                          product_id SERIAL PRIMARY KEY,
+                          name VARCHAR(100),
+                          stock INT,
+                          price NUMERIC(10,2)
+);
+
+CREATE TABLE orders (
+                        order_id SERIAL PRIMARY KEY,
+                        customer_id INT REFERENCES customers(customer_id),
+                        total_amount NUMERIC(12,2),
+                        created_at TIMESTAMP DEFAULT NOW(),
+                        status VARCHAR(20) DEFAULT 'PENDING'
+);
+
+CREATE TABLE order_items (
+                             item_id SERIAL PRIMARY KEY,
+                             order_id INT REFERENCES orders(order_id),
+                             product_id INT REFERENCES products(product_id),
+                             quantity INT,
+                             subtotal NUMERIC(10,2)
+);
 
 DO $$
-DECLARE
-    v_customer_id INT;
-    v_balance NUMERIC(12,2);
-    v_total NUMERIC(12,2) := 0;
-    v_order_id INT;
-    v_price NUMERIC(10,2);
-    v_stock INT;
-BEGIN
-    SELECT customer_id, balance INTO v_customer_id, v_balance
-    FROM customers
-    WHERE name = 'Tran Thi B'
-    FOR UPDATE;
+        DECLARE
+            v_customer_id INT := 1;
+            v_balance INT;
+            v_order_id INT;
+            v_subtotal_1 NUMERIC(12, 2);
+            v_subtotal_2 NUMERIC(12, 2);
+            v_total_amount NUMERIC(12, 2);
+            v_stock_1 INT;
+            v_stock_2 INT;
+            v_price_1 NUMERIC;
+            v_price_2 NUMERIC;
+        begin
+            Select c.balance INTO v_balance
+            FROM customers c
+            WHERE c.customer_id = v_customer_id
+            FOR UPDATE;
 
-    SELECT price, stock INTO v_price, v_stock FROM products WHERE product_id = 1 FOR UPDATE;
-    IF v_stock < 1 THEN
-        RAISE EXCEPTION 'OUT_OF_STOCK';
-    END IF;
-    v_total := v_total + v_price * 1;
+            SELECT stock, price INTO v_stock_1, v_price_1
+            FROM products
+            WHERE product_id = 1;
 
-    SELECT price, stock INTO v_price, v_stock FROM products WHERE product_id = 3 FOR UPDATE;
-    IF v_stock < 2 THEN
-        RAISE EXCEPTION 'OUT_OF_STOCK';
-    END IF;
-    v_total := v_total + v_price * 2;
+            SELECT stock, price INTO v_stock_2, v_price_2
+            FROM products
+            WHERE product_id = 3;
 
-    IF v_balance < v_total THEN
-        RAISE EXCEPTION 'INSUFFICIENT_BALANCE';
-    END IF;
+            IF v_stock_1 < 1 OR v_stock_2 < 2 THEN
+                RAISE EXCEPTION 'Không đủ hàng. Vui lòng quay lại khi khác!';
+            end if;
 
-    INSERT INTO orders(customer_id, total_amount, status)
-    VALUES (v_customer_id, v_total, 'PENDING')
-    RETURNING order_id INTO v_order_id;
+            v_subtotal_1 := v_price_1 * 1;
+            v_subtotal_2 := v_price_2 * 2;
+            v_total_amount := v_subtotal_1 + v_subtotal_2;
 
-    UPDATE products SET stock = stock - 1 WHERE product_id = 1;
-    INSERT INTO order_items(order_id, product_id, quantity, subtotal)
-    VALUES (v_order_id, 1, 1, (SELECT price FROM products WHERE product_id = 1) * 1);
+            IF v_balance < v_total_amount THEN
+                RAISE EXCEPTION 'Số dư của bạn không đủ. Vui lòng kiểm tra lại!';
+            end if;
 
-    UPDATE products SET stock = stock - 2 WHERE product_id = 3;
-    INSERT INTO order_items(order_id, product_id, quantity, subtotal)
-    VALUES (v_order_id, 3, 2, (SELECT price FROM products WHERE product_id = 3) * 2);
+            INSERT INTO orders (customer_id, total_amount, created_at)
+            VALUES (1, v_total_amount, now())
+            RETURNING order_id INTO v_order_id;
 
-    UPDATE customers SET balance = balance - v_total WHERE customer_id = v_customer_id;
+            INSERT INTO order_items (order_id, product_id, quantity, subtotal)
+            VALUES
+                (v_order_id, 1, 1, v_subtotal_1),
+                (v_order_id, 3, 2, v_subtotal_2);
 
-    UPDATE orders SET status = 'COMPLETED' WHERE order_id = v_order_id;
+            UPDATE customers
+            SET balance = balance - v_total_amount
+            WHERE customer_id = v_customer_id;
 
-END $$;
+            UPDATE products
+            SET stock = stock - 1
+            WHERE product_id = 1;
 
-COMMIT;
+            UPDATE products
+            SET stock = stock - 2
+            WHERE product_id = 3;
+
+            UPDATE orders
+            SET status = 'COMPLETED'
+            WHERE order_id = v_order_id;
+        end
+    $$ language plpgsql;
+
+
+SELECT * FROM orders;
+SELECT * FROM order_items;
+SELECT * FROM customers;
+SELECT * FROM products;
+
+CREATE SCHEMA xs_2;
