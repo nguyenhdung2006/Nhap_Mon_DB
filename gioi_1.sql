1e35ff02381bdbb47b8af43ad068027af085196b
Nguyen Huy Dung <234769026+nguyenhdung2006@users.noreply.github.com>
2026-04-15 11:08:22 +0700

    change content

--- gioi_1.sql
+++ gioi_1.sql
@@ -1,29 +1,52 @@
 BEGIN;
 
-UPDATE products SET stock = stock - 2 WHERE product_id = 1;
-UPDATE products SET stock = stock - 1 WHERE product_id = 2;
+-- 1. Kiểm tra tồn kho
+DO $$
+    DECLARE
+        stock_p1 INT;
+        stock_p2 INT;
+        price_p1 NUMERIC;
+        price_p2 NUMERIC;
+        orderId INT;
+        total NUMERIC := 0;
+    BEGIN
+        -- Lấy tồn kho + giá
+        SELECT stock, price INTO stock_p1, price_p1 FROM products WHERE product_id = 1;
+        SELECT stock, price INTO stock_p2, price_p2 FROM products WHERE product_id = 2;
 
-INSERT INTO orders (customer_name, total_amount) 
-VALUES ('Nguyen Van A', (SELECT price * 2 FROM products WHERE product_id = 1) + (SELECT price * 1 FROM products WHERE product_id = 2))
-RETURNING order_id;
+        -- Nếu thiếu hàng → rollback
+        IF stock_p1 < 2 OR stock_p2 < 1 THEN
+            RAISE EXCEPTION 'Không đủ hàng trong kho!';
+        END IF;
 
-INSERT INTO order_items (order_id, product_id, quantity, subtotal)
-VALUES 
-(currval('orders_order_id_seq'), 1, 2, (SELECT price * 2 FROM products WHERE product_id = 1)),
-(currval('orders_order_id_seq'), 2, 1, (SELECT price * 1 FROM products WHERE product_id = 2));
+        -- 2. Trừ kho
+        UPDATE products SET stock = stock - 2 WHERE product_id = 1;
+        UPDATE products SET stock = stock - 1 WHERE product_id = 2;
+
+        -- 3. Tạo order
+        INSERT INTO orders(customer_name)
+        VALUES ('Nguyen Van A')
+        RETURNING order_id INTO orderId; -- lấy ngay dữ liệu từ insert/update/delete thay vì select
+
+        --SELECT order_id
+        --FROM orders
+        --WHERE customer_name = 'Nguyen Van A'
+        --ORDER BY order_id DESC
+        --LIMIT 1;
+
+        -- 4. Thêm order_items
+        INSERT INTO order_items(order_id, product_id, quantity, subtotal)
+        VALUES
+            (orderId, 1, 2, price_p1 * 2),
+            (orderId, 2, 1, price_p2 * 1);
+
+        -- 5. Tính tổng tiền
+        total := price_p1 * 2 + price_p2 * 1;
+
+        UPDATE orders
+        SET total_amount = total
+        WHERE order_id = orderId;
+
+    END $$;
 
 COMMIT;
-
-UPDATE products SET stock = 0 WHERE product_id = 1;
-
-BEGIN;
-
-UPDATE products SET stock = stock - 2 WHERE product_id = 1;
-
-UPDATE products SET stock = stock - 1 WHERE product_id = 2;
-
-ROLLBACK;
-
-SELECT * FROM products;
-SELECT * FROM orders;
-SELECT * FROM order_items;
