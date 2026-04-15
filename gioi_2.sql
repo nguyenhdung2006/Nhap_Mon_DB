3727bcf11d2a3dcdfed0ddccca242723977a300f
Nguyen Huy Dung <234769026+nguyenhdung2006@users.noreply.github.com>
2026-04-15 10:47:47 +0700

    change content

--- gioi_2.sql
+++ gioi_2.sql
@@ -1,24 +1,81 @@
+CREATE TABLE accounts (
+    account_id SERIAL PRIMARY KEY,
+    customer_name VARCHAR(100),
+    balance NUMERIC(12,2)
+);
+
+CREATE TABLE transactions (
+                              trans_id SERIAL PRIMARY KEY,
+                              account_id INT REFERENCES accounts(account_id),
+                              amount NUMERIC(12,2),
+                              trans_type VARCHAR(20), -- 'WITHDRAW-rut tien' hoặc 'DEPOSIT-nap tien'
+                              created_at TIMESTAMP DEFAULT NOW()
+);
+
+BEGIN ;
+
+DO $$
+    DECLARE
+        v_account_id INT := 1;
+        v_amount NUMERIC := 100;
+        v_balance NUMERIC;
+    BEGIN
+
+        -- 1. lấy balance
+        SELECT balance
+        INTO v_balance
+        FROM accounts
+        WHERE account_id = v_account_id
+        FOR UPDATE;
+
+        -- 2. kiểm tra đủ tiền
+        IF v_balance < v_amount THEN
+            RAISE EXCEPTION 'Không đủ tiền';
+        END IF;
+
+        -- 3. trừ tiền
+        UPDATE accounts
+        SET balance = balance - v_amount
+        WHERE account_id = v_account_id;
+
+        -- 4. ghi log transaction
+        INSERT INTO transactions(account_id, amount, trans_type)
+        VALUES (v_account_id, v_amount, 'WITHDRAW');
+
+END $$ language plpgsql;
+
+COMMIT ;
+
 BEGIN;
 
-UPDATE accounts 
-SET balance = balance - 500.00 
-WHERE account_id = 1 AND balance >= 500.00;
+DO $$
+    DECLARE
+        v_account_id INT := 1;
+        v_amount NUMERIC := 100;
+        v_balance NUMERIC;
+    BEGIN
 
-INSERT INTO transactions (account_id, amount, trans_type) 
-VALUES (1, 500.00, 'WITHDRAW');
+        -- 1. lấy balance
+        SELECT balance
+        INTO v_balance
+        FROM accounts
+        WHERE account_id = v_account_id
+            FOR UPDATE;
 
-COMMIT;
+        -- 2. kiểm tra
+        IF v_balance < v_amount THEN
+            RAISE EXCEPTION 'Không đủ tiền';
+        END IF;
 
-BEGIN;
+        -- 3. trừ tiền
+        UPDATE accounts
+        SET balance = balance - v_amount
+        WHERE account_id = v_account_id;
 
-UPDATE accounts 
-SET balance = balance - 200.00 
-WHERE account_id = 1 AND balance >= 200.00;
+        -- ❌ 4. CỐ Ý GÂY LỖI: account_id = 999999 không tồn tại
+        INSERT INTO transactions(account_id, amount, trans_type)
+        VALUES (999999, v_amount, 'WITHDRAW');
 
-INSERT INTO transactions (account_id, amount, trans_type) 
-VALUES (999, 200.00, 'WITHDRAW');
+    END $$;
 
 ROLLBACK;
-
-SELECT * FROM accounts;
-SELECT * FROM transactions;
